7467135bf690b4c4e2f11b80da3624cd0f6e0efe
Sameer Khan <115018276+Sameeerkhan78@users.noreply.github.com>
2023-02-25 12:21:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,22 @@
-import React from "react";
-const Todo = ({ id, value, dispatch }) => {
-  return (
-    <div className="todo">
-      <div className="todo-title">{value}   <button
-        className="todo-delete"
-        onClick={() => dispatch({ type: "delete_todo", payload: { id: id } })}
-      >
-        Delete
-      </button></div>
-   
-    </div>
-  );
-};
 
-export { Todo };
+import React, { useReducer } from 'react'
+import { todoReducer } from '../reducers/todo';
+import '../styles/App.css';
+import { AddTodo } from './AddTodo';
+import { Todo } from './Todo';
+const App = () => {
+  const [state,dispatch] = useReducer(todoReducer,[])
+  
+ 
+  return (
+    <div id="main">
+      <AddTodo dispatch={dispatch} state={state} />
+      <Todo dispatch={dispatch} state={state}/>
+     {/* Render list of Todo Components here */}
+    </div>
+  )
+}
+
+
+
+export default App;
